93e63a7f6161944bb080954e925bae8b74bc931e
skammala <skammala01@gmail.com>
2023-09-10 12:02:35 -0600

    linking app to Firebase project

---
+++ bicycle-map/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'bicycle-map-e7ea7',
+    appId: '1:331599006637:web:3c5ba27e1f3f7318622f24',
+    storageBucket: 'bicycle-map-e7ea7.appspot.com',
+    apiKey: 'AIzaSyCgJSg7uQYsTyhE2M_7rxcBZ9XZPIKrpRE',
+    authDomain: 'bicycle-map-e7ea7.firebaseapp.com',
+    messagingSenderId: '331599006637',
+    measurementId: 'G-T5GP7HLPZ1',
+  },
+};
